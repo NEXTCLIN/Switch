49f3cfec7097dc848bb162311752d6b3af9f8f97
Odair Gonçalves de Jesus <odairj@hotmail.com>
2018-11-21 13:55:44 -0200

    Configuração do Contexto

---
+++ Switch.Domain/Enums/SexoEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Enums
+{
+    public enum SexoEnum
+    {
+        feminino = 2 ,
+        masculino = 3
+    }
+}
