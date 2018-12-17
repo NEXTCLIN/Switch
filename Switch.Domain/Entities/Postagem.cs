c045b2c7d39fc4ac6240677dc6353b409940655c
Odair Gonçalves de Jesus <odairj@hotmail.com>
2018-12-17 01:07:42 -0200

    Atualização 17/12/2018

---
+++ Switch.Domain/Entities/Postagem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Entities
+{
+    public class Postagem
+    {
+        public int Id { get; set; }
+        public DateTime DataPublicacao { get; set; }
+        public String Texto { get; set; }
+
+    }
+}
